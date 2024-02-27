6a9b4aaf9d3e3cc6a1af88722b0e37e0d568efc1
philippe-hjik <philippe.heijkoop@eduvaud.ch>
2024-02-27 16:32:24 +0100

    création de route basique

---
+++ src/router/routes.mjs
@@ -0,0 +1,29 @@
+// routes.mjs
+
+import express from 'express';
+const router = express.Router();
+
+// Route de get
+router.get('/home', (req, res) => {
+
+
+});
+
+// Route de post
+router.post('/', (req, res) => {
+
+});
+
+// Route de update
+router.put('/', (req, res) => {
+
+});
+
+// Route de delete
+router.delete('/', (req, res) => {
+
+});
+
+
+// Exporter le routeur
+export { router };
