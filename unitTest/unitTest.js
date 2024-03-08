92cb254fe29e874ef2e6a061d4be577084850ce7
philippe-hjik <122774959+philippe-hjik@users.noreply.github.com>
2024-03-08 08:47:53 +0100

    Update unitTest.js

--- unitTest/unitTest.js
+++ unitTest/unitTest.js
@@ -5,13 +5,9 @@ import { sequelize } from "./sequelize/sequelize.mjs"; // Assurez-vous d'ajuster
 async function testDatabaseConnection() {
   try {
     // Connexion à la base de données
-    await sequelize.authenticate();
     console.log("Connexion à la base de données établie avec succès !");
   } catch (error) {
     console.error("Impossible de se connecter à la base de données :", error);
-  } finally {
-    // Fermeture de la connexion à la base de données
-    await sequelize.close();
   }
 }
 
