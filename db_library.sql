f113eea56a2bd4f9055071b1eb269be162f20e96
philippe-hjik <122774959+philippe-hjik@users.noreply.github.com>
2024-04-22 09:22:18 +0200

    modification du script sql

--- db_library.sql
+++ db_library.sql
@@ -1,3 +1,17 @@
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+CREATE DATABASE IF NOT EXISTS `db_library` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `db_library`;
+
+DROP TABLE IF EXISTS `t_users`;
+CREATE TABLE `t_users` ( `id` int PRIMARY KEY AUTO_INCREMENT, `username` varchar(255) NOT NULL, `password` varchar(255) NOT NULL, `nbBooksOffer` int, `nbNote` int, `nbComment` int, `created` datetime, `updatedAt` datetime );ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 /*
 #################### Update Part (t_users) ####################
 */
