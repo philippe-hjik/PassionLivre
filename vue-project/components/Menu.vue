f34a78f4f28e9bff76e5fe66cd1c4afbf4569cbf
philippe-hjik <122774959+philippe-hjik@users.noreply.github.com>
2024-04-29 10:33:28 +0200

    changement de page avec le menuBar est fonctionnel grace a url

--- vue-project/components/Menu.vue
+++ vue-project/components/Menu.vue
@@ -1,36 +1,31 @@
 <template>
-    <div class="menu">
-      <Menubar :model="items" />
-    </div>
-  </template>
-  
-  <script>
-  import Menubar from 'primevue/menubar';
-  
-  export default {
-    components: {
-      Menubar
-    },
-    data() {
-      return {
-        items: [
-          {
-            label: 'Home',
-            icon: 'pi pi-home',
-            route: '/asd'
-          },
-          {
-            label: 'Features',
-            icon: 'pi pi-user'
-          }]
-      };
-    }
+  <div class="menu">
+    <Menubar :model="items" />
+  </div>
+</template>
+
+<script>
+import Menubar from 'primevue/menubar';
+export default {
+  components: {
+    Menubar
+  },
+  data() {
+    return {
+      items: [
+        {
+          label: 'Home',
+          icon: 'pi pi-home',
+          url: '/'
+        },
+        {
+          label: 'Login',
+          icon: 'pi pi-user',
+          url: '/login'
+        }]
+    };
   }
-  </script>
-  
-  <style scoped>
-  body {
-    background-color: #f8fafc;
-  }
-  </style>
-  
+}
+</script>
+
+<style scoped></style>
